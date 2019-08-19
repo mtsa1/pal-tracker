be1fc0fff47ed67325325eb43f94a1e60f8859b9
pal_user <mtsai@kr.af.mil>
2019-08-19 15:30:27 -0400

    configuring the appgit add -pgit add -pgit add -p

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,41 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+public class EnvController {
+
+
+    private String port;
+    private String memLimit;
+    private String instanceIndex;
+    private String instanceAddr;
+
+    public EnvController(
+            @Value("${port: SET}") String port,
+            @Value("${memory.limit:NOT SET}") String memLimit,
+            @Value("${cf.instance.index:NOT SET}") String instanceIndex,
+            @Value("${cf.instance.addr:NOT SET}") String instanceAddr) {
+        this.port = port;
+        this.memLimit = memLimit;
+        this.instanceIndex = instanceIndex;
+        this.instanceAddr = instanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+        Map<String, String> environments = new HashMap<String, String>();
+
+        environments.put("PORT", port);
+        environments.put("MEMORY_LIMIT", memLimit);
+        environments.put("CF_INSTANCE_INDEX", instanceIndex);
+        environments.put("CF_INSTANCE_ADDR", instanceAddr);
+
+        return environments;
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public String helloString = "hello";
+
+
+    public WelcomeController(@Value("${welcome.message}") String helloString) {
+        this.helloString = helloString;
+    }
+
+
     @GetMapping
     public String sayHello(){
-        return "hello";
+        return helloString;
     }
 
 }
