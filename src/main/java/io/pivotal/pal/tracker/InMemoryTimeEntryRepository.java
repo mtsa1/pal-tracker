96aa7bf6ec584cc83afaadb85f69d960fd66380b
pal_user <mtsai@kr.af.mil>
2019-08-21 14:57:27 -0400

    JDBC Template

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@Repository
+
 public class InMemoryTimeEntryRepository implements TimeEntryRepository {
 
     private HashMap<Long, TimeEntry> repository = new HashMap<Long, TimeEntry>();
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
